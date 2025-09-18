1a5a7e0a4e710d7b891502eccf9c21b2237ccc71
Josemiranda989 <josemiranda989@gmail.com>
2025-09-18 17:35:38 -0300

    sequelize done

--- controller/products.controller.js
+++ controller/products.controller.js
@@ -1,26 +1,91 @@
+const db = require("../database/models/index");
+const fs = require('fs')
+const path = require('path');
+const { where } = require("sequelize");
+
 module.exports = {
     // Muestra el prod especifico
-    detail: function (req, res, next) {
-        res.render("products/detail");
+    detail: async function (req, res, next) {
+        try {
+            const product = await db.Product.findByPk(req.params.id, {
+                include: "genre",
+            });
+            // res.json(product)
+            res.render("products/detail", { product });
+        } catch (error) {
+            console.log(error);
+        }
     },
     // Muestra el formulario para crear
-    create: function (req, res, next) {
-        res.render("products/create");
+    create: async function (req, res, next) {
+        try {
+            const genres = await db.Genre.findAll();
+            res.render("products/create", { genres });
+        } catch (error) {
+            console.log(error);
+        }
     },
     // Permite crear un prod
-    processCreate: function (req, res, next) {
-   
+    processCreate: async function (req, res, next) {
+        try {
+            await db.Product.create({
+                name: req.body.name,
+                description: req.body.description,
+                image: req.file?.filename || "default-image.jpeg",
+                genre_id: req.body.genre,
+            });
+
+            res.redirect("/");
+        } catch (error) {
+            console.log(error);
+        }
     },
     // Muestra el formulario para editar
-    update: function (req, res, next) {
-        res.render("products/update");
+    update: async function (req, res, next) {
+        try {
+            const prod = await db.Product.findByPk(req.params.id)
+            const genres = await db.Genre.findAll();
+            res.render("products/update", { genres, prod });
+        } catch (error) {
+            console.log(error);
+        }
     },
     // Permite actualizar el prod
-    processUpdate: function (req, res, next) {
+    processUpdate: async function (req, res, next) {
+            try {
+                await db.Product.update(
+                    {
+                        name: req.body.name,
+                        description: req.body.description,
+                        image: req.file?.filename,
+                        genre_id: req.body.genre,
+                    }, {
+                        where: {
+                            id:req.params.id
+                        }
+                    }
+                )
 
+                res.redirect('/')
+            } catch (error) {
+                console.log(error)
+            }
     },
     // Permite eliminar el prod
-    processDelete: function (req, res, next) {
-
+    processDelete: async function (req, res, next) {
+        try {
+            const prodToDelete = await db.Product.findByPk(req.params.id)
+            if(prodToDelete.image != 'default-image.jpeg'){
+                fs.unlinkSync(path.join(__dirname, '../public/images/products/' + prodToDelete.image))
+            }
+            await db.Product.destroy({
+                where: {
+                    id: req.params.id,
+                },
+            });
+            res.redirect("/");
+        } catch (error) {
+            console.log(error);
+        }
     },
 };
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -7,18 +7,21 @@ const {
     update,
     processDelete,
 } = require("../controller/products.controller");
+const upload = require("../middlewares/multer");
 var router = express.Router();
+
+
 
 // Muestra el prod especifico
 router.get("/detail/:id", detail);
 // Muestra el formulario para crear
 router.get("/create", create);
 // Permite crear un prod
-router.post("/create", processCreate);
+router.post("/create", upload.single('image'), processCreate);
 // Muestra el formulario para editar
 router.get('/update/:id', update )
 // Permite actualizar el prod
-router.put('/update/:id', processUpdate )
+router.put('/update/:id', upload.single('image'), processUpdate )
 // Permite eliminar el prod
 router.delete('/delete/:id', processDelete )
 
